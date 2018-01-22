446408bfa704f2745b05301411f283ece0d6f417
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-22 14:49:19 -0500

    Create Register.php

---
+++ engine/Register.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is a part of Nenglish7/Nenglish.Me.
+ *
+ * @author Nicholas English                   <https://github.com/Nenglish7>.
+ * @author Nenglish7/Nenglish.Me Contributors <Nenglish7/Nenglish.Me/graphs/contributors>.
+ *
+ * @copyright 2018-2018 Nicholas English.
+ *
+ * @link    <https://github.com/Nenglish7/Nenglish.Me> Github Repository.
+ * @license <https://choosealicense.com/no-permission/> No License.
+ */
+namespace Nenglish7\Engine;
+/**
+ * Register.
+ */
+class Register
+{
+
+}
